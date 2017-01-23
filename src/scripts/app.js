fa2c3803a062cb9a0e7157f6e21a2ec2fbbad208
Jesse Showalter <iamjesseshowalter@gmail.com>
2017-01-23 12:08:51 -1000

    added vertical tabs in 2 column layout

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,23 @@
-// app.js
+// @codekit-prepend "vendors/jquery-3.1.1.min.js";
+
+$(document).ready(function(){
+
+
+  // vertical Tabs for Press and FAQ
+  $(".vertical-tab-content").hide();
+  $(".vertical-tab-content:first").show();
+
+  $(".vertical-tab").click(function(event) {
+    event.preventDefault();
+
+    $(".vertical-tab-content").hide();
+    var activeTab = $(this).attr("rel");
+    $("#"+activeTab).show();
+
+    $(".vertical-tab").removeClass("sideNavActive");
+    $(this).addClass("sideNavActive");
+
+  });
+
+
+});

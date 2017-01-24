8505ad927a5a4f6bb98a473ddf4bf3f210ac2b69
Jesse Showalter <iamjesseshowalter@gmail.com>
2017-01-23 18:13:36 -1000

    so much, so much
    installed sliders, finished initial home page, as well as all tabs and
    toggle content

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,11 @@
 // @codekit-prepend "vendors/jquery-3.1.1.min.js";
+// @codekit-prepend "vendors/slick.js";
 
 $(document).ready(function(){
 
-
+  // -------------------------------
   // vertical Tabs for Press and FAQ
+  // -------------------------------
   $(".vertical-tab-content").hide();
   $(".vertical-tab-content:first").show();
 
@@ -16,8 +18,28 @@ $(document).ready(function(){
 
     $(".vertical-tab").removeClass("sideNavActive");
     $(this).addClass("sideNavActive");
-
   });
 
+  // -------------------------------
+  // Slider
+  // -------------------------------
+  $('.slider').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 6500,
+    pauseOnHover: true,
+    arrows: false,
+  });
+  $('.flyerSlider').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 6500,
+    pauseOnHover: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+  });
 
 });

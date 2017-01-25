79f92ea1564dbba7f12bb3c83a3b641a81e469a4
Jesse Showalter <iamjesseshowalter@gmail.com>
2017-01-24 17:27:24 -1000

    added scroll to top for side nav

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -65,6 +65,15 @@ $(document).ready(function(){
     })
   })();
 
+ // --------------------------------------------
+ //  Back To Top
+ // --------------------------------------------
+ //Click event to scroll to top
+ $('.scrollToTop').click(function(){
+   $('html, body').animate({scrollTop : 0},800);
+   return false;
+ });
+
 
 
 });
